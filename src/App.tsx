3a4f4611484db5a006acc0e8c8cfdb591b59eb55
unknown <juleskevinnjiki@gmail.com>
2022-08-31 16:06:57 +0100

    contact_adminpage_accueil

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import { HashRouter as Router } from "react-router-dom";
 import HomePage from "./components/Home";
+import Page_accueil from "./pages/Page_accueil";
 
 function App() {
   return (
     <div className="app">
         <Router>
-          <HomePage />
+          <Page_accueil />
         </Router>
     </div>
   );
